87f8b59c509fe53f85f1b657642a68e15caa547a
marmotcai <marmotcai@163.com>
2019-02-22 15:53:43 +0800

    准备加入对docker的部署支持，但由于centosdockerDBus问题还未能成功

--- ceph/script/copy_ssh_id.sh
+++ ceph/script/copy_ssh_id.sh
@@ -24,32 +24,33 @@ if  [[ ${USERNAME} = 'root' ]]; then
 fi
 
 cat hosts | while read ip port host pwd param; do
-  sshpass -p $pwd ssh-copy-id -i ${WORK_DIR}/.ssh/id_rsa.pub -f ${USERNAME}@$ip 2>/dev/null
+  echo "copy ssh id to $ip:$port"
+  sshpass -p $pwd ssh-copy-id -i ${WORK_DIR}/.ssh/id_rsa.pub -f ${USERNAME}@$ip -p $port 2>/dev/null
   
-  if  [[ ${USERNAME} = 'root' ]]; then
-    ssh -nq ${USERNAME}@$ip "hostnamectl set-hostname $host"
-    ssh -nq ${USERNAME}@$ip "echo -e 'y\n' | ssh-keygen -q -f /root/.ssh/id_rsa -t rsa -N ''"
+  if [[ ${USERNAME} = 'root' ]]; then
+    ssh -nq -p $port $ip "hostnamectl set-hostname $host"
+    ssh -nq -p $port $ip "echo -e 'y\n' | ssh-keygen -q -f /root/.ssh/id_rsa -t rsa -N ''"
     echo "===== Copy id_rsa.pub of $ip ====="
-    scp $ip:/root/.ssh/id_rsa.pub ./$host-id_rsa.pub
+    scp -P $port $ip:/root/.ssh/id_rsa.pub ./$host-id_rsa.pub
     cat ./$host-id_rsa.pub >> ${keysfile}
     
     echo $ip $host >> /etc/hosts
   else
-    sshpass -p $pwd ssh-copy-id -i ${WORK_DIR}/.ssh/id_rsa.pub -f root@$ip 2>/dev/null
-    sudo sshpass -p $pwd ssh-copy-id -i /root/.ssh/id_rsa.pub -f ${USERNAME}@$ip 2>/dev/null
+    sshpass -p $pwd ssh-copy-id -i ${WORK_DIR}/.ssh/id_rsa.pub -f root@$ip -p $port 2>/dev/null
+    sudo sshpass -p $pwd ssh-copy-id -i /root/.ssh/id_rsa.pub -f ${USERNAME}@$ip -p $port 2>/dev/null
   fi
 done
-
+exit 0
 if  [[ ${USERNAME} = 'root' ]]; then
   cat /root/.ssh/id_rsa.pub >> ${keysfile}
   cat hosts | while read ip port host pwd param; do
     rm -f ./$host-id_rsa.pub
     echo "===== Copy authorized_keys to $ip ====="
-    scp ${keysfile} $ip:/root/.ssh/
-    scp /etc/hosts $ip:/etc/
-    scp /etc/ssh/ssh_config @$ip:/etc/ssh/ssh_config
-    scp /etc/ssh/sshd_config $ip:/etc/ssh/sshd_config
-    ssh -nq $ip "systemctl restart sshd"
+    scp ${keysfile} -P $port $ip:/root/.ssh/
+    scp /etc/hosts -P $port $ip:/etc/
+    scp /etc/ssh/ssh_config -P $port $ip:/etc/ssh/ssh_config
+    scp /etc/ssh/sshd_config -P $port $ip:/etc/ssh/sshd_config
+    ssh -nq -p $port $ip "systemctl restart sshd"
   done
 fi
 
