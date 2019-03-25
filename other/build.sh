3e8c1c03cc17e22653755376ea1c44fb2d24c177
caijun <caijun@cloudgather.cn>
2019-03-25 17:15:07 +0800

    auto commit 2019-03-25:17:15:07

--- other/build.sh
+++ other/build.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-docker build -t marmotcai/minio .
+cmd=${1}
+port=${2}
+access_key=${3}
+secret_key=${4}
+case $cmd in 
+    server)
+       docker rm -f my-minio-server
+       docker run -p $port:9000 --name my-minio-server \
+                  -e "MINIO_ACCESS_KEY=$access_key" \
+                  -e "MINIO_SECRET_KEY=$secret_key" \
+                  minio/minio server /data
+    ;;
 
-docker rm -f my-minio
-docker run -p 9000:9000 marmotcai/minio server /data
-# docker run -d -v $PWD/data:/data -p 9000:9000 --name my-minio marmotcai/minio
+    nas)
+       docker rm -f my-minio-nas
+       docker run -p $port:9000 --name my-minio-nas \
+                  -e "MINIO_ACCESS_KEY=$access_key" \
+                  -e "MINIO_SECRET_KEY=$secret_key" \
+                  -v $PWD/nasvol:/container/vol \
+                  minio/minio gateway nas /container/vol
+    ;;
+
+    build)
+      docker build -t marmotcai/minio .
+    ;;
+
+    *)
+      echo "use: sh build.sh build"
+      echo "use: sh build.sh server port access_key secret_key"
+      echo "use: sh build.sh nas port access_key secret_key"
+    ;;
+
+esac
+
