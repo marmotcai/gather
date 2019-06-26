42468dfa26610dcc7e081188dc58427d45d30da3
docker builder <docker@atoml.com>
2019-06-26 17:28:38 +0800

    auto commit 2019-06-26:17:28:38

---
+++ opencv/opencv2.sh
@@ -0,0 +1,4 @@
+export PATH="/usr/local/opt/opencv@2/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/opencv@2/lib"
+export CPPFLAGS="-I/usr/local/opt/opencv@2/include"
+export PKG_CONFIG_PATH="/usr/local/opt/opencv@2/lib/pkgconfig"
---
+++ opencv/opencv3.sh
@@ -0,0 +1,4 @@
+export PATH="/usr/local/opt/opencv@3/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/opencv@3/lib"
+export CPPFLAGS="-I/usr/local/opt/opencv@3/include"
+export PKG_CONFIG_PATH="/usr/local/opt/opencv@3/lib/pkgconfig"
