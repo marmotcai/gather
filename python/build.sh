10721d8fb3247bab7fa8acbaa59c696d21e0731a
caijun <marmotcai@163.com>
2020-02-03 15:06:15 +0800

    modify the python Dockerfile

--- python/build.sh
+++ python/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# docker build --target python-base -t marmotcai/python-base -f ./Dockerfile .
-# docker rm -f my-python-base
-# docker run -p 1722:22 -p 1788:8888 -v $PWD/data:/root/data --name my-python-base -d marmotcai/python-base 
+docker build --target python-base -t marmotcai/python-base -f ./Dockerfile .
+docker rm -f my-python-base
+docker run -p 1622:22 -v $PWD/data:/root/data --name my-python-base -d marmotcai/python-base 
 
-docker build --target python-jupyter-notebook -t marmotcai/python-jupyter-notebook -f ./Dockerfile .
-docker rm -f my-python-jupyter-notebook
-docker run -p 1722:22 -p 1788:8888 -v $PWD/data:/root/data --name my-python-jupyter-notebook -d marmotcai/python-jupyter-notebook 
+# docker build --target python-jupyter-notebook -t marmotcai/python-jupyter-notebook -f ./Dockerfile .
+# docker rm -f my-python-jupyter-notebook
+# docker run -p 1722:22 -p 1788:8888 -v $PWD/data:/root/data --name my-python-jupyter-notebook -d marmotcai/python-jupyter-notebook 
 
